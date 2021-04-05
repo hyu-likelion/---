1a00826830e866ebb96d399bd4d1e50842128424
hyunthree2 <42177358+hyunthree2@users.noreply.github.com>
2021-04-05 23:27:33 +0900

    Delete week1 directory

--- week1/problem4.py
+++
@@ -1,8 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    completion.append('')
-    for x in range(0,len(participant)):
-        if participant[x] != completion[x]:
-            return(participant[x])
-            break
