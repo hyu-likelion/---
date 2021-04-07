21dec9d3341c531eae5bb0154b267c62394c18eb
jeongmin1217 <balljm@naver.com>
2021-04-07 20:21:03 +0900

    session

---
+++ week1-session/problem1.py
@@ -0,0 +1,9 @@
+def student_func(x):
+    if x % 3 == 0 or x % 5 == 0:
+        return threefive
+    elif x % 3 == 0:
+        return three
+    elif x % 5 == 0:
+        return five
+    else:
+        return x
